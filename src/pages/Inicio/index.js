358a6162ef0d4dd84134180a21eeb3f5a2e08514
Carlos Alan <cardosocarlosalan@gmail.com>
2023-06-24 11:36:04 -0300

    create card component

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,5 +1,6 @@
 import Banner from "componentes/Banner";
 import Cabecalho from "componentes/Cabecalho";
+import Card from "componentes/Card";
 import Rodape from "componentes/Rodape";
 import Titulo from "componentes/Titulo";
 import React from "react";
@@ -12,6 +13,7 @@ export default function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
+      <Card id= '1' titulo='Carlos Alan' capa='https://st.depositphotos.com/1003989/3639/i/450/depositphotos_36394689-stock-photo-business-woman-hold-credit.jpg' />
       <Rodape />
     </>
   );

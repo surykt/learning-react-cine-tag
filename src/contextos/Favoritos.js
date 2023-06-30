c536698eb5d501598098ed00719da95f6a4d9525
Carlos Alan <cardosocarlosalan@gmail.com>
2023-06-29 22:32:20 -0300

    create favoritos context hook

--- src/contextos/Favoritos.js
+++ src/contextos/Favoritos.js
@@ -1,14 +1,38 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useContext, useState } from 'react'
 
 export const FavoritosContext = createContext()
+FavoritosContext.displayName = 'Favoritos'
 
 export default function FavoritosProvider({ children }) {
 
-  const [favoritos, setFavoritos] = useState([])
+  const [favorito, setFavorito] = useState([])
 
   return(
-    <FavoritosContext.Provider value={{favoritos, setFavoritos}}>
+    <FavoritosContext.Provider value={{favorito, setFavorito}}>
       {children}
     </FavoritosContext.Provider>
   )
+}
+
+export function useFavoritoContext() {
+  const { favorito, setFavorito } = useContext(FavoritosContext)
+
+  function adicionarFavorito(novoFavorito) {
+    const favoritoRepetido = favorito.some(item => item.id === novoFavorito.id)
+
+    let novaLista = [...favorito]
+
+    if(!favoritoRepetido) {
+      novaLista.push(novoFavorito)
+      return setFavorito(novaLista)
+    }
+
+    novaLista = favorito.filter(favorito => favorito.id !== novoFavorito.id )
+    return setFavorito(novaLista)
+  }
+
+  return {
+    favorito,
+    adicionarFavorito,
+  }
 }
